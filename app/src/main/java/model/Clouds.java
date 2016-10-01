2876ec883e96e877a454158ab783b308a7dfd18f
csd713 <jeevi146@gmail.com>
2016-10-01 02:21:49 -0400

    Models

---
+++ app/src/main/java/model/Clouds.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by Chiru on 10/1/2016.
+ */
+
+public class Clouds {
+    private int precepitation;
+
+    public int getPrecepitation() {
+        return precepitation;
+    }
+
+    public void setPrecepitation(int precepitation) {
+        this.precepitation = precepitation;
+    }
+}
---
+++ app/src/main/java/model/Snow.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by Chiru on 10/1/2016.
+ */
+
+public class Snow {
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+
+    private int precipitation;
+}
