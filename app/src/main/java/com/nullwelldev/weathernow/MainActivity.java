5379596a853c465fd409b4722750a3d862518e3d
csd713 <jeevi146@gmail.com>
2016-10-01 02:20:30 -0400

    Override a method

--- app/src/main/java/com/nullwelldev/weathernow/MainActivity.java
+++ app/src/main/java/com/nullwelldev/weathernow/MainActivity.java
@@ -3,6 +3,7 @@ package com.nullwelldev.weathernow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         //Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /**Handle action bar item clicks here. the action bar will
+         * automatically handle clicks on to the Home/Up button, so long as you
+         * specify a parent activity in AndroidManifest.xml.
+          */
+        int id = item.getItemId();
+        if(id == R.id.changeCityId){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
