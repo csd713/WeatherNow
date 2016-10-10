38935511f1d4da52a81c5c9fddf35f6fcdb96716
csd713 <jeevi146@gmail.com>
2016-10-09 20:37:40 -0400

    Added to read data from API

--- app/src/main/java/com/nullwelldev/weathernow/MainActivity.java
+++ app/src/main/java/com/nullwelldev/weathernow/MainActivity.java
@@ -1,17 +1,80 @@
 package com.nullwelldev.weathernow;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.TextureView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import data.JSONWeatherParser;
+import data.WeatherHttpClient;
+import model.Weather;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cityName;
+    private TextView temp;
+    private ImageView iconView;
+    private TextView description;
+    private TextView humidity;
+    private TextView pressure;
+    private TextView wind;
+    private TextView sunrise;
+    private TextView sunset;
+    private TextView updated;
+
+    Weather weather = new Weather();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityName = (TextView) findViewById(R.id.cityText);
+        iconView = (ImageView) findViewById(R.id.thumbnailIcon);
+        temp = (TextView) findViewById(R.id.temperatureText);
+        description = (TextView) findViewById(R.id.cloudText);
+        humidity = (TextView) findViewById(R.id.humidText);
+        pressure = (TextView) findViewById(R.id.pressureText);
+        wind = (TextView) findViewById(R.id.windText);
+        sunrise = (TextView) findViewById(R.id.riseText);
+        sunset = (TextView) findViewById(R.id.setText);
+        updated = (TextView) findViewById(R.id.updateText);
+
+        renderWeatherData("Syracuse,US");
     }
+
+
+    public void renderWeatherData(String city){
+        WeatherTask weatherTask = new WeatherTask();
+        weatherTask.execute(new String[]{city + "&units=metric"});
+    }
+    private class WeatherTask extends AsyncTask<String, Void, Weather>{
+
+        @Override
+        protected Weather doInBackground(String... params) {
+
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            weather = JSONWeatherParser.getWeather(data);
+
+            Log.v("Data:", weather.place.getCity());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather){
+
+            super.onPostExecute(weather);
+        }
+
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
